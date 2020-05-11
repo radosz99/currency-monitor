1da08bd7ead647e4d75aed353bad0d81da08ac40
radosz99 <47248979+radosz99@users.noreply.github.com>
2020-05-11 23:20:40 +0200

    Move repository from GitLab

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# currency-monitor
+# General info
+Aplikacja zapisuje do bazy danych kursy walut z [tej](https://www.x-rates.com/table/?from=PLN&amount=1) strony, porównuje je z ostatnimi zanotowanymi wartościami oraz wypisuje odpowiedni log na konsoli.
+
---
+++ src/main.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from datetime import datetime
+import data.connection as db
+import data.raport as rap
+import data.currency as cur
+
+
+def get_data(data):
+    name = ''
+    value = 0
+    parts = data.split('td')
+    try:
+        parts_name = parts[1].split('<')
+    except IndexError:
+        return
+    name = parts_name[0][1:]
+    parts_value = parts[5].split('</a>')
+    parts_value = parts_value[0].split('>')
+    value = parts_value[len(parts_value)-1]
+    return name, value
+
+conn = db.get_connection()
+current_day = datetime.now().strftime("%Y-%m-%d")
+current_hour = datetime.now().strftime("%H:%M")
+raport_id = rap.insert_raport(current_day, current_hour, conn)
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome('resources/chromedriver.exe', options=options)
+driver.get("https://www.x-rates.com/table/?from=PLN&amount=1")
+el = driver.find_elements_by_xpath("//tr")
+for e in el[1:]:
+    try:
+        name, value = (get_data(e.get_attribute('innerHTML')))
+        value = str(float(value))
+    except TypeError:
+        continue   
+    old_value = cur.insert_currency(raport_id, name, value, conn)
+    if old_value is not None:
+        print(f'Waluta {name}, stara wartość {old_value}, nowa wartość {value}')
+        if(str(old_value) != value):
+            print(f'Nastąpiła zmiana, różnica to {float(value)-float(old_value)}')
+        else:
+            print('Nic się nie zmieniło')
